0e578227b8843b70111bd0efd455b8719c524e41
wonseok <bulnabang99@gmail.com>
2023-05-25 17:59:27 +0900

    Daily Algorithm

---
+++ _2023_05/day_05_25/BJ_2307_도로검문.java
@@ -0,0 +1,18 @@
+package com.ssafy._2023_05.day_05_25;
+
+public class BJ_2307_도로검문 {
+    class Node {
+        int idx;
+        int dist;
+
+        public Node(int idx, int dist) {
+            this.idx = idx;
+            this.dist = dist;
+        }
+    }
+
+    // 경찰이 검문하는 정점을 제외한 목적지까지의 최단 경로를 구하여라!
+    public static void main(String[] args) {
+
+    }
+}
